1cc5d002ab83a5b5a82abe3790b27b9f2517e17e
joeba <joebahpou5@gmail.com>
2021-12-31 01:27:09 +0200

    working on api

--- award.py
+++ award.py
@@ -1,3 +1,6 @@
+import db_connector
+
+
 def addAwardQuery(year, category_id, movie_id, has_won):
     return """INSERT INTO award (year, oscar_category_id, movie_id, has_won)
      VALUES (%s,%s,%s,%s)""" % \
@@ -7,9 +10,11 @@ def addAwardQuery(year, category_id, movie_id, has_won):
 def addAwardPersonQuery(award_id, person_id):
     return """INSERT INTO award_person (award_id,person_id) VALUES(%s,%s)""" % (award_id, person_id)
 
-def getMovieAwardYear():
-    pass
-
+def getMovieAwardYear(movie_id):
+    query =("SELECT year FROM award WHERE movie_id= (%s) ") % movie_id
+    award_1= db_connector.getFromDB(query,1)
+    award_year=award_1[0][0]
+    return award_year
 
 class Award:
     def __init__(self, year, category_id, movie_id, has_won, person=None, award_id=0):
--- retrieve_from_api.py
+++ retrieve_from_api.py
@@ -5,6 +5,7 @@ import string
 import mysql
 import csv
 
+import award
 from categories import *
 import movie
 import person
@@ -17,10 +18,14 @@ def retrieve_movies_and_cast():
     addJobsToDB()
 
     num_of_movies = movie.getMoviesCount()
+    movie_title=""
     for i in range(num_of_movies):
         movie_ = movie.getMoviesByID(i)
-        movie_title=movie_.title
+        movie_title=str(movie_.title)
+        movie_title=movie_title.replace(" ","%20")
         movie_sql_id=movie_.id
+        nomination_year=award.getMovieAwardYear(movie_sql_id)
+        link= "https://api.themoviedb.org/3/search/movie?api_key="+api_key+ "&language=en-US&query="+movie_title +"&page=1&include_adult=true&year="+nomination_year+"&primary_release_year="+nomination_year
 
 
 
