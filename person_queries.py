c88e05f0459bde1c892c44e5c87a99bba253743a
Udi Ronen <udironen@mail.tau.ac.il>
2022-01-07 01:05:30 +0200

    added person web app implementation

--- person_queries.py
+++ person_queries.py
@@ -5,50 +5,97 @@ def getBasicPersonInfo(person_name):
     query = f"""SELECT person.name, person.gender FROM person WHERE person.name = '{person_name}'"""
     response = db_connector.getFromDB(query)
     if len(response) > 0:
+        print(response[0])
         return response[0]
     return None
 
 
-def getPersonMovies(person_name, only_win=False, only_nominated=False, genres=[], categories=[],
-                    min_year=1934, max_year=2010):
-    query = getQueryPersonMovies(person_name, only_win=only_win, only_nominated=only_nominated,
-                                 genres=genres, categories=categories, min_year=min_year, max_year=max_year)
+def getNumOfOscarNominations(person_name, genres=[], categories=[],
+                             min_year=1934, max_year=2010):
+    return getCountPersonNominations(person_name, False, True, genres, categories, min_year, max_year)
+
+
+def getNumOfMovies(person_name, genres=[], categories=[],
+                   min_year=1934, max_year=2010):
+    return getCountPersonNominations(person_name, False, False, genres, categories, min_year, max_year)
+
+
+def getNumOfWins(person_name, genres=[], categories=[],
+                 min_year=1934, max_year=2010):
+    return getCountPersonNominations(person_name, True, True, genres, categories, min_year, max_year)
+
+
+def getPersonNominations(person_name, categories=[], min_year=1934, max_year=2010):
+    query = getQueryNominationsByPerson(person_name, categories=categories, min_year=min_year, max_year=max_year)
     response = db_connector.getFromDB(query)
     for tup in response:
         print(tup)
     return response
 
 
-def getCountPersonMovies(person_name, only_win=False, only_nominated=False, genres=[], categories=[],
-                         min_year=1934, max_year=2010):
-    query = getQueryPersonMovies(person_name, only_win=only_win, only_nominated=only_nominated,
-                                 genres=genres, categories=categories, min_year=min_year, max_year=max_year)
-    query_count = f"""SELECT COUNT(*) FROM ({query}) AS personMovies"""
+def getPersonMovies(person_name):
+    query = getQueryMoviesByPerson(person_name)
+    response = db_connector.getFromDB(query)
+    for tup in response:
+        print(tup)
+    return response
+
+
+def getCountPersonNominations(person_name):
+    query = getQueryNominationsByPerson(person_name)
+    query_count = f"""SELECT COUNT(*) FROM ({query}) AS personNominations"""
     response = db_connector.getFromDB(query_count)
+    print(response[0][0])
     return response[0][0]
 
 
-def getQueryPersonMovies(person_name, only_win=False, only_nominated=False, genres=[], categories=[]
-                         , min_year=1934, max_year=2010):
-    query = f""" SELECT distinct movie.title AS movieTitle, jobInMovie.job_name, oscarCategory.category AS category, 
-                            award.year AS year, award.has_won AS won 
-                    FROM    person, award, award_person, oscarCategory, movie , person_movie_job, jobInMovie
-                            {getQueryFrom_Movie_Genre(len(genres) > 0)}
+def getCountPersonMovies(person_name):
+    query = getQueryMoviesByPerson(person_name)
+    query_count = f"""SELECT COUNT(*) FROM ({query}) AS personMovies"""
+    response = db_connector.getFromDB(query_count)
+    print(response[0][0])
+    return response[0][0]
+
+
+def getCountPersonWins(person_name):
+    query = getQueryNominationsByPerson(person_name)
+    query_count = f"""SELECT COUNT(*) FROM ({query}) AS personMovies WHERE won = 1"""
+    response = db_connector.getFromDB(query_count)
+    print(response[0][0])
+    return response[0][0]
+
+
+def getQueryMoviesByPerson(person_name):
+    query = f"""    SELECT  distinct    movie.title     AS movieTitle,
+                                        jobInMovie.job_name  As role
+                    FROM    movie, jobInMovie, 
+                            person_movie_job,
+                            person
+                    WHERE   person.name = "{person_name}"
+                    AND     person.id = person_movie_job.person_id                                   
+                    AND     person_movie_job.job_id = jobInMovie.id                                   
+                    AND     person_movie_job.movie_id = movie.id                                   
+                    """
+    return query
+
+
+def getQueryNominationsByPerson(person_name, categories=[]
+                                , min_year=1934, max_year=2010):
+    query = f"""    SELECT  movie.title             AS movieTitle,
+                            oscarCategory.category  AS category, 
+                            award.year              AS year,
+                             award.has_won          AS won 
+                    FROM    person, award, oscarCategory, movie, award_person
                     WHERE   person.name = "{person_name}"                
                     AND     oscarCategory.id = award.oscar_category_id 
                     AND     movie.id = award.movie_id
-                    AND     person_movie_job.person_id = person.id
-                    AND     person_movie_job.movie_id = movie.id
-                    AND     person_movie_job.job_id = jobInMovie.id
+                    AND     person.id = award_person.person_id
+                    AND     award_person.award_id = award.id
                     """
-    query += "{0}{1}{2}{3}{4}{5}{6}" \
-        .format(getQueryJoinMovieGenre(len(genres) > 0),
-                getQueryGenres(genres),
-                getQueryCategories(categories),
+    query += "{0}{1}{2}" \
+        .format(getQueryCategories(categories),
                 getQueryAwardMinYear(min_year),
-                getQueryAwardMaxYear(max_year),
-                getQueryOnlyNominated(only_nominated),
-                getQueryWin(only_win))
+                getQueryAwardMaxYear(max_year))
     return query
 
 
@@ -75,6 +122,12 @@ def getQueryJoinMovieGenre(join):
 def getQueryFrom_Movie_Genre(addFrom):
     if addFrom:
         return ", movie_genre\n"
+    return ""
+
+
+def getQueryFrom_Award_Person(addFrom):
+    if addFrom:
+        return ", award_person\n"
     return ""
 
 
