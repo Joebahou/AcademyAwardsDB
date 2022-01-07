5435003c45158d7694bd1cdbbe59a7e160260933
joeba <joebahpou5@gmail.com>
2022-01-08 01:30:04 +0200

    little correction

--- SRC/CREATE_DB_SCRIPT/create_db_indices.py
+++ SRC/CREATE_DB_SCRIPT/create_db_indices.py
@@ -6,9 +6,9 @@ def create_fulltext_indices():
 
 
 def create_indices():
-    person_index="""CREATE  INDEX name_idx ON person(name) using hash"""
-    award_index = """CREATE  INDEX award_idx ON award(id,year) """
-    movie_index="""CREATE  INDEX title_idx ON movie(title) using hash """
+    person_index="""CREATE  INDEX name_idx ON person(name) USING HASH """
+    award_index = """CREATE  INDEX award_idx ON award(id,year) USING BTREE """
+    movie_index="""CREATE  INDEX title_idx ON movie(title) USING HASH  """
     movie_job_index = """CREATE  INDEX movie_job_idx ON person_movie_job(movie_id,job_id) """
     db_connector.insertToDB(person_index)
     db_connector.insertToDB(award_index)
