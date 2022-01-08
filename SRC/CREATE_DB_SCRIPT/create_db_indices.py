30c9548903b2ee7d75c49f4d83a715cbec6b7a27
joeba <joebahpou5@gmail.com>
2022-01-08 02:17:30 +0200

    little correction

--- SRC/CREATE_DB_SCRIPT/create_db_indices.py
+++ SRC/CREATE_DB_SCRIPT/create_db_indices.py
@@ -9,7 +9,7 @@ def create_indices():
     person_index="""CREATE  INDEX name_idx ON person(name) USING HASH """
     award_index = """CREATE  INDEX award_idx ON award(id,year) USING BTREE """
     movie_index="""CREATE  INDEX title_idx ON movie(title) USING HASH  """
-    movie_job_index = """CREATE  INDEX movie_job_idx ON person_movie_job(movie_id,job_id) """
+    movie_job_index = """CREATE  INDEX movie_job_idx ON person_movie_job(movie_id,job_id) USING HASH"""
     db_connector.insertToDB(person_index)
     db_connector.insertToDB(award_index)
     db_connector.insertToDB(movie_index)
